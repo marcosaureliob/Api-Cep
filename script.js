f2d02afb327c21036ed8d4598ca335fc52c12b11
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-03-13 12:21:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+async function buscaEndereco(cep){
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if(consultaCepConvertida.erro) {
+            throw Error('CEP não existente')
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro)
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
